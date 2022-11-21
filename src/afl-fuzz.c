a62c9abce11ba01bf815a771e4b61643eab93c92
sjmluo <simonjluo@gmail.com>
2022-11-21 16:36:09 +1100

    remove rl_params

--- src/afl-fuzz.c
+++ src/afl-fuzz.c
@@ -1551,11 +1551,11 @@ int main(int argc, char **argv_orig, char **envp) {
 #ifdef CALCULATE_OVERHEAD
     t1 = get_timestamp();
     overhead += (t1 - t0) / 1000000.0;
-    OKF("Seed scehduler overhead is: %.4g, %.4g", overhead / ((t1 - T0) / 1000000.0), afl->rl_params->update_overhead_sec );
+    OKF("Seed scehduler overhead is: %.4g, %.4g", overhead / ((t1 - T0) / 1000000.0), afl->update_overhead_sec );
      
 
 
-    scheduler_overhead = alloc_printf("%f, %f, %f\n", (double) (t1 - T0) / 1000000.0, (double) overhead, afl->rl_params->update_overhead_sec );
+    scheduler_overhead = alloc_printf("%f, %f, %f\n", (double) (t1 - T0) / 1000000.0, (double) overhead, afl->update_overhead_sec );
 
     write((int) (fd), scheduler_overhead, strlen(scheduler_overhead));
 #endif
